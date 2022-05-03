98ca7c0c0eaf8cacc2ff173a21bf9704a153228d
yakov <iakovboiev@gmail.com>
2022-05-03 16:44:05 +0300

    create APi

--- backend/meduzzenapp/models.py
+++ backend/meduzzenapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    register_data = models.DateField("register data", auto_now_add=True)
+
--- backend/meduzzenapp/views.py
+++ backend/meduzzenapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import UserModelSerializer
 
-# Create your views here.
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+
+    def perform_create(self, serializer):
+        if 'password' in self.request.data:
+            password = make_password(self.request.data['password'])
+            serializer.save(password=password)
+        else:
+            serializer.save()
